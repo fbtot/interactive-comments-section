986f8fc0ea0760ac1c8779c84cc1a0d890668d97
Filippo Bristot <filippo.bristot@hotmail.it>
2022-01-19 08:57:26 +0100

    feat(js): add json to localStorage

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+async function getJSONComments() {
+  const response = await fetch('./data.json');
+  const jsonResponse = response.json();
+  return jsonResponse;
+}
 
+function addJSONtoLocalStorage() {
+  getJSONComments().then((comments) => localStorage.setItem('fmComments', JSON.stringify(comments)));
+}
+
+addJSONtoLocalStorage();
